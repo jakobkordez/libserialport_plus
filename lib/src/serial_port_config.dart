47d102672e96e30c95d58b232ca999b625372aab
Jakob Kordež <jakobkordez1999@gmail.com>
2025-04-25 21:03:04 +0200

    Add toString method for SerialPortConfig

--- lib/src/serial_port_config.dart
+++ lib/src/serial_port_config.dart
@@ -26,6 +26,22 @@ class SerialPortConfig extends Equatable {
   @override
   List<Object?> get props =>
       [baudRate, bits, parity, stopBits, rts, cts, dtr, dsr, xonXoff];
+
+  @override
+  String toString() {
+    final props = [
+      'baudRate: $baudRate',
+      'bits: $bits',
+      'parity: ${parity?.name}',
+      'stopBits: $stopBits',
+      'rts: ${rts?.name}',
+      'cts: ${cts?.name}',
+      'dtr: ${dtr?.name}',
+      'dsr: ${dsr?.name}',
+      'xonXoff: ${xonXoff?.name}',
+    ];
+    return 'SerialPortConfig(${props.join(', ')})';
+  }
 }
 
 enum SerialPortParity {
